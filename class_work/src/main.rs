876499abc6df491cdd924c2831db48e0a6af51a2
Alex-rod-c <alexandro.rodriguez02@utrgv.edu>
2025-09-24 20:07:36 +0000

    in_class_2

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,4 +1,19 @@
-fn con_string(s1: &String, s2: &String) -> String
+fn modify_clone(s: &String) -> String{
+    let mut cloned = s.clone();
+    cloned.push_str("world!");
+    cloned
+}
+
+fn main(){
+    let s = String::from("Hello, ");
+    let modified = modify_clone(&s);
+    println!("Orginal: {}", s);
+    println!("Modified: {}", modified);
+}
+
+
+
+/*fn con_string(s1: &String, s2: &String) -> String
 {
     let mut result = String::new();
     result.push_str(s1);
@@ -13,5 +28,6 @@ fn main() {
     let s2 = String::from("world!");
     let result = con_string(&s1, &s2);
     println!("{}", result);
-}
+ }
+\*
 
