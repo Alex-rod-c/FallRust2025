152236a1e2dba9dc36d61e0f2c609627a341f740
Alex-rod-c <alexandro.rodriguez02@utrgv.edu>
2025-09-24 19:53:23 +0000

    assignment_1

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn con_string(s1: &String, s2: &String) -> String
+{
+    let mut result = String::new();
+    result.push_str(s1);
+    result.push_str(s2);
+    result
 }
+
+
+
+fn main() {
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let result = con_string(&s1, &s2);
+    println!("{}", result);
+}
+
