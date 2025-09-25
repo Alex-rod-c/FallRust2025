2ecb3118e101f5ac873cf06de1453124ef70bd91
Alex-rod-c <alexandro.rodriguez02@utrgv.edu>
2025-09-25 23:56:31 +0000

    Homework1 1-2

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,19 +1,46 @@
-const freeze_F: f64 = 32.0;
-
-fn far_2_cel(f: f64) -> f64 {
-    (f - 32.0) * 5.0 / 9.0
-}
-
-fn cel_2_far(c: f64) -> f64{
-    (c * 9.0 / 5.0) + 32.0
+fn even_num(n: i32) -> bool {
+    n % 2 == 0
 }
 
 fn main(){
-    let mut temp_f: f64 = freeze_F;
-    pintln!("{:.1}°F = {:.1}°C", temp_f, far_2_cel(temp_f));
+    let numbers: [i32; 10] [3, 5, 10, 15, 22, 33, 45, 60, 71,, 99];
 
-    for i in 1..=5 {
-        temp_f += 1.0;
-        println!("{:.1}°F = {:.1}°", temp_f, far_2_cel(temp_f));
+    for &n in numbers.iter(){
+        if n % 15 == 0{
+            println!("{} -> Fizzbuzz", n);
+        }
+        else if n % 3 == 0{
+            println!("{} -> Fizz", n);
+        }
+        else if n % 5 == 0 {
+            println("{} -> Buzz",n);
+        }
+        else if even_num(n){
+            println("{} -> Even", n);
+        }
+        else{
+            printin("{} -> odd", n);
+        }
     }
+
+    let mut sum = 0
+    let mut i =0;
+    while i < numbers.len(){
+        sum += numbers[i];
+        i += 1;
+    }
+    println!("\nsum of all numbers = {}", sum);
+
+    let mut largest = numbers[0];
+    let mut j =1;
+    loop{
+        if j >= numbers.len(){
+            break;
+        }
+        if numbers[j] > largest {
+            largest = numbers[j];
+        }
+        j += 1;
+    }
+    println!("Largest number = {}", largest);
 }
