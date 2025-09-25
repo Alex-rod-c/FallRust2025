1ac098e6245ce818190b3e398e9baab6adf40f16
Alex-rod-c <alexandro.rodriguez02@utrgv.edu>
2025-09-25 23:26:05 +0000

    Homework1 1-1

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,15 +1,19 @@
-fn sun(total: &mut i32, low: i32, high: i32){
-    *total_sum = 0;
-    for i in low..=high{
-        *total_sum += i;
+const freeze_F: f64 = 32.0;
+
+fn far_2_cel(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+fn cel_2_far(c: f64) -> f64{
+    (c * 9.0 / 5.0) + 32.0
+}
+
+fn main(){
+    let mut temp_f: f64 = freeze_F;
+    pintln!("{:.1}°F = {:.1}°C", temp_f, far_2_cel(temp_f));
+
+    for i in 1..=5 {
+        temp_f += 1.0;
+        println!("{:.1}°F = {:.1}°", temp_f, far_2_cel(temp_f));
     }
 }
-
-fn main()
-{
-    let mup total_sum = 0
-    sum(&mut total_sum, 0, 100);
-    println!("Total sum from 0 to 100 is: {}", total_sum)
-}
-
-
