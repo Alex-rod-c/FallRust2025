5b7bc064aed93836bffbf790da3eb9767222fcbd
Alex-rod-c <alexandro.rodriguez02@utrgv.edu>
2025-09-26 01:03:51 +0000

    homework1 1-3

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,46 +1,41 @@
-fn even_num(n: i32) -> bool {
-    n % 2 == 0
+fn guess_check(guess: i32, secret: i32) -> i32{
+    if guess == secret{
+        0
+    } 
+    else if furess > secret {
+        1
+    }
+    else{
+        -1
+    }
 }
 
-fn main(){
-    let numbers: [i32; 10] [3, 5, 10, 15, 22, 33, 45, 60, 71,, 99];
+fn main()[
+    let secret: i32 = 42;
 
-    for &n in numbers.iter(){
-        if n % 15 == 0{
-            println!("{} -> Fizzbuzz", n);
-        }
-        else if n % 3 == 0{
-            println!("{} -> Fizz", n);
-        }
-        else if n % 5 == 0 {
-            println("{} -> Buzz",n);
-        }
-        else if even_num(n){
-            println("{} -> Even", n);
-        }
-        else{
-            printin("{} -> odd", n);
-        }
-    }
+    let guesses: [i32; 5] = [10,50,30,42,60];
 
-    let mut sum = 0
-    let mut i =0;
-    while i < numbers.len(){
-        sum += numbers[i];
-        i += 1;
-    }
-    println!("\nsum of all numbers = {}", sum);
+    let mut attempts = 0;
+    let mut index = 0;
 
-    let mut largest = numbers[0];
-    let mut j =1;
-    loop{
-        if j >= numbers.len(){
-            break;
-        }
-        if numbers[j] > largest {
-            largest = numbers[j];
-        }
-        j += 1;
+    let guess = guesses[index];
+    attempts += 1;
+
+    let result = guess_check(guess, secret);
+loop{
+    if result == 0{
+        println("Guess {} is correct!", guess);
+        break
     }
-    println!("Largest number = {}", largest);
+    else if result == 1{
+        prinin("guess {} is to high!", guess);
+
+    }
+    else{
+        println!("Guess {} is too low!", guess)
+    }
+    index += 1;
 }
+
+println!("It took {} guesses.", attempts);
+]
