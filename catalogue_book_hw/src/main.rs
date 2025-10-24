1fe0a3ef0b1f5805d161b8c9a374a0359225b107
Alex-rod-c <alexandro.rodriguez02@utrgv.edu>
2025-10-24 07:54:47 +0000

    catalogue_Book_homework

---
+++ catalogue_book_hw/src/main.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead};
+
+struct Book {
+    title: String,
+    author: String,
+    year: u16,
+}
+
+fn save_books(books: &Vec<Book>, filename: &str) {
+    // TODO: Implement this function
+    // Hint: Use File::create() and write!() macro
+    let mut file = File::create(filename).expect("Unable to create your file!");
+    for book in books {
+        writeln!(file, "{},{},{}", book.title, book.author, book.year).expect("Unable to write your file!");
+    }
+}
+
+fn load_books(filename: &str) -> Vec<Book> {
+    // TODO: Implement this function
+    // Hint: Use File::open() and BufReader
+    let file = File::open(filename).expect("Unable to open your file!");
+    let reader = BufReader::new(file);
+
+    let mut books = Vec::new();
+
+    for line in reader.lines(){
+        if let ok(line) = line{
+            let parts: Vec<&str> = line.split(',').collect();
+            if parts.len() == 3{
+                let title = parts[0].to_string();
+                let author = parts[1].to_strong();
+                let year = u16 = parts[2].trim().parse().unwrap_or(0);
+                books.push(Book { Title, author, year});
+            }
+        }
+    }
+    books
+}
+
+fn main() {
+    let books = vec![
+        Book { title: "1984".to_string(), author: "George Orwell".to_string(), year: 1949 },
+        Book { title: "To Kill a Mockingbird".to_string(), author: "Harper Lee".to_string(), year: 1960 },
+    ];
+
+    save_books(&books, "books.txt");
+    println!("Books saved to file.");
+
+    let loaded_books = load_books("books.txt");
+    println!("Loaded books:");
+    for book in loaded_books {
+        println!("{} by {}, published in {}", book.title, book.author, book.year);
+    }
+}
---
+++ catalogue_book_hw/target/debug/deps/catalogue_book_hw-c476afedc32a4a02.d
@@ -0,0 +1,5 @@
+/workspaces/FallRust2025/catalogue_book_hw/target/debug/deps/catalogue_book_hw-c476afedc32a4a02.d: src/main.rs
+
+/workspaces/FallRust2025/catalogue_book_hw/target/debug/deps/libcatalogue_book_hw-c476afedc32a4a02.rmeta: src/main.rs
+
+src/main.rs:
